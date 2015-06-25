95d952bee5dd77ce35896bd87252b1e4863dec69
Yoni Teitelbaum <ytbaum@gmail.com>
2015-06-25 11:15:40 -0400

    first chunk of code, project.clj, and .gitignore

---
+++ src/app/core.clj
@@ -0,0 +1,25 @@
+(ns app.core)
+
+(def colors #{"red" "green" "blue" "orange" "white" "yellow"})
+(def nturns 10)
+(def ncolors 4)
+(def should-play true)
+
+(defn get-combo [colors ncolors]
+  (repeatedly ncolors (fn [] (rand-nth (vec colors)))))
+
+(defn get-color-map [colors]
+  (let [first-letters (map (fn [color] (str (get color 0))) colors)]
+    (zipmap first-letters colors))) 
+
+; grabbed this from:
+; http://blog.jayfields.com/2011/01/clojure-select-keys-select-values-and.html
+(defn select-values [map ks]
+         (reduce #(conj %1 (map %2)) [] ks))
+
+(defn get-user-guess [colors]
+  (println "Enter your guess:")
+  (let [guess (read-line)
+        color-map (get-color-map colors) ]
+    (select-values color-map (map str (vec guess)))))  
+
