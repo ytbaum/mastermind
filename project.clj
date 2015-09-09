141725514c6a0e95298df74d337bd23f64529cbc
Yoni Teitelbaum <ytbaum@gmail.com>
2015-09-09 16:33:43 -0400

    some more changes necessary to run lein jar

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject app "1.0.0-SNAPSHOT"
+(defproject app "1.0.0"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                  [seesaw "1.4.5"]])
+                  [seesaw "1.4.5"]]
+  :main app.core
+  :manifest {"Class-Path" "lib/clojure-1.4.0.jar"})
